953671126c4838085a0e5ec71c4a0d969714b4bd
herkommer <m@herkommer.co.uk>
2018-10-26 08:52:59 +0200

    First happy path working, yay!

---
+++ AcmeCalculator/Calculate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeCalculator
+{
+    public class Calculate : ICalculator
+    {
+        public decimal Add(decimal number1, decimal number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
---
+++ AcmeCalculator/ICalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeCalculator
+{
+    public interface ICalculator
+    {
+        decimal Add(decimal number1, decimal number2);
+    }
+}
